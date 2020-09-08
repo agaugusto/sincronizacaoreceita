3a21807653b77797eb1eb92c7bd70964e39aa568
adriano <adrianogaugusto@gmail.com>
2020-09-08 01:58:50 -0300

    Inclusão de testes

--- src/test/java/br/com/adriano/sincronizacaoreceita/services/SincronizacaoReceitaServiceTest.java
+++ src/test/java/br/com/adriano/sincronizacaoreceita/services/SincronizacaoReceitaServiceTest.java
@@ -1,7 +1,30 @@
 package br.com.adriano.sincronizacaoreceita.services;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
 
 public class SincronizacaoReceitaServiceTest {
 
+    LeitorCSVService leitorCSVServiceSpy;
+    SincronizacaoReceitaService service;
+
+    @Before
+    public void setup(){
+        leitorCSVServiceSpy = spy(new LeitorCSVService());
+        service = new SincronizacaoReceitaService(leitorCSVServiceSpy);
+    }
+
+    @Test
+    public void deveProcessarComSucesso(){
+        service.processarAtualizacao("/home/adriano/workspace/sincronizacaoreceita/src/test/import/contas.csv");
+        verify(leitorCSVServiceSpy, times(4)).BuscarProximaConta();
+    }
+
+    @Test
+    public void naoDeveProcessarQuandoNaoForUmArquivoValido(){
+        service.processarAtualizacao("");
+        verify(leitorCSVServiceSpy, never()).BuscarProximaConta();
+    }
 }
